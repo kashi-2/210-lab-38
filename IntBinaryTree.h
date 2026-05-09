1f54cb6be2052057e0bff0d73ae73f4ea949b450
Akashdeep <akashdeep.thind.singh@gmail.com>
2026-05-08 20:43:17 -0700

    Milestone 2

--- IntBinaryTree.h
+++ IntBinaryTree.h
@@ -1,6 +1,10 @@
 // Specification file for the IntBinaryTree class
 #ifndef INTBINARYTREE_H
 #define INTBINARYTREE_H
+#include <string>
+#include <iostream>
+using namespace std;
+
 
 // The IntBinaryTree class manages a binary tree of integers.
 class IntBinaryTree {
