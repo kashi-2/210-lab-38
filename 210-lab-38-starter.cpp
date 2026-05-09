de7100218ef6a9cff8cca12773ea2264ebb17abc
Akashdeep <akashdeep.thind.singh@gmail.com>
2026-05-08 21:08:24 -0700

    Milestone 3

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "IntBinaryTree.h"
 using namespace std;
 
+// Function prototypes
+void load_file(IntBinaryTree &tree);
+
 int main() {
-    
+    IntBinaryTree tree;
+
+    load_file(tree);
+
+    cout << "\nBST Inorder Traversal:\n";
+    tree.displayInOrder();
 
     return 0;
+}
+
+// Load records from file
+void load_file(IntBinaryTree &tree) {
+    ifstream fin("codes.txt");
+    string code;
+
+    if (!fin) {
+        cout << "Error opening file.\n";
+        return;
+    }
+
+    while (fin >> code) {
+        tree.insertNode(code);
+    }
+
+    fin.close();
+
+    cout << "Codes loaded into BST successfully.\n";
 }
